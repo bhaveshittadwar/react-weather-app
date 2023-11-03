3ff0561b203ad6dbfd45e63e11fc53a4a511cc85
bhaveshittadwar <bitsadwar@gmail.com>
2023-11-03 02:34:17 -0400

    feat: setup geoloacation search

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a59f7b9628mshc6a18546cec2007p12590cjsn21da437af344',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
